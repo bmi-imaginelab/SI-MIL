417781dc8367642366f9712985997876286329ce
Saarthak Kapse <44542181+saarthak-kapse@users.noreply.github.com>
2024-05-01 19:40:49 -0400

    Update sample_train_mil.sh

--- train_mil.sh
+++ train_mil.sh
@@ -14,12 +14,23 @@ run_script() {
 	echo "Running with hyperparameters: $1, $2, $3, $4, $5, $6, $7"
 	
 	# SI-MIL (ours)
-    python train.py  --torch_seed -1  --dataset_split_path 'train_5x_dict.pickle' --dataset_split_path_test 'test_5x_dict.pickle'   --dataset_split_deep_path 'train_5x_dict_dino.pickle' --dataset_split_deep_path_test 'test_5x_dict_dino.pickle' --features_deep_path 'trainfeat_dino.pth'  --features_deep_path_test 'testfeat_dino.pth' --features_path 'binned_hcf.csv' --save_path '/MIL_experiment/ABMIL' --dropout_patch $1 --num_epochs $2 --weight_decay $3 --lr $4 --top_k $6  --organ '' --gpu_index 1    --use_additive 'yes'  --dropout_node $5  --no_projection 'yes'   --feat_type 'patchlevelattention_with_featattn_mlpmixer_attnreg_Lmse'  --stop_gradient 'no'  --cross_val_fold $7 --temperature 3.0	 &
+    python train.py  --torch_seed -1  --dataset_split_path 'train_5x_dict.pickle' --dataset_split_path_test 'test_5x_dict.pickle'   --dataset_split_deep_path 'train_5x_dict_dino.pickle' --dataset_split_deep_path_test 'test_5x_dict_dino.pickle' --features_deep_path 'trainfeat_dino.pth'  --features_deep_path_test 'testfeat_dino.pth' --features_path 'binned_hcf.csv' --save_path '/MIL_experiment/ABMIL' --dropout_patch $1 --num_epochs $2 --weight_decay $3 --lr $4 --top_k $6  --organ '' --gpu_index 1    --use_additive 'yes'  --dropout_node $5  --no_projection 'yes'   --feat_type 'patchattn_with_featattn_mlpmixer_Lmse'  --stop_gradient 'no'  --cross_val_fold $7 --temperature 3.0	 &
 	# Lmse for Knowledge distillation
 	
 	
 	# w/o PAG Top-K since stop_gradient is yes
-    python train.py  --torch_seed -1  --dataset_split_path 'train_5x_dict.pickle' --dataset_split_path_test 'test_5x_dict.pickle'   --dataset_split_deep_path 'train_5x_dict_dino.pickle' --dataset_split_deep_path_test 'test_5x_dict_dino.pickle' --features_deep_path 'trainfeat_dino.pth'  --features_deep_path_test 'testfeat_dino.pth' --features_path 'binned_hcf.csv' --save_path '/MIL_experiment/ABMIL' --dropout_patch $1 --num_epochs $2 --weight_decay $3 --lr $4 --top_k $6  --organ '' --gpu_index 1    --use_additive 'yes'  --dropout_node $5  --no_projection 'yes'   --feat_type 'patchlevelattention_with_featattn_mlpmixer_attnreg_Lmse'  --stop_gradient 'yes'  --cross_val_fold $7 --temperature 3.0	 &
+    python train.py  --torch_seed -1  --dataset_split_path 'train_5x_dict.pickle' --dataset_split_path_test 'test_5x_dict.pickle'   --dataset_split_deep_path 'train_5x_dict_dino.pickle' --dataset_split_deep_path_test 'test_5x_dict_dino.pickle' --features_deep_path 'trainfeat_dino.pth'  --features_deep_path_test 'testfeat_dino.pth' --features_path 'binned_hcf.csv' --save_path '/MIL_experiment/ABMIL' --dropout_patch $1 --num_epochs $2 --weight_decay $3 --lr $4 --top_k $6  --organ '' --gpu_index 1    --use_additive 'yes'  --dropout_node $5  --no_projection 'yes'   --feat_type 'patchattn_with_featattn_mlpmixer_Lmse'  --stop_gradient 'yes'  --cross_val_fold $7 --temperature 3.0	 &
+
+
+	# w/o KD since feat_type doesn't has Lmse in it's name
+    python train.py  --torch_seed -1  --dataset_split_path 'train_5x_dict.pickle' --dataset_split_path_test 'test_5x_dict.pickle'   --dataset_split_deep_path 'train_5x_dict_dino.pickle' --dataset_split_deep_path_test 'test_5x_dict_dino.pickle' --features_deep_path 'trainfeat_dino.pth'  --features_deep_path_test 'testfeat_dino.pth' --features_path 'binned_hcf.csv' --save_path '/MIL_experiment/ABMIL' --dropout_patch $1 --num_epochs $2 --weight_decay $3 --lr $4 --top_k $6  --organ '' --gpu_index 1    --use_additive 'yes'  --dropout_node $5  --no_projection 'yes'   --feat_type 'patchattn_with_featattn_mlpmixer'  --stop_gradient 'no'  --cross_val_fold $7 --temperature 3.0	 &
+
+
+	# w/o PAG Top-K and KD since stop_gradient is yes and feat_type doesn't has Lmse in it's name
+    python train.py  --torch_seed -1  --dataset_split_path 'train_5x_dict.pickle' --dataset_split_path_test 'test_5x_dict.pickle'   --dataset_split_deep_path 'train_5x_dict_dino.pickle' --dataset_split_deep_path_test 'test_5x_dict_dino.pickle' --features_deep_path 'trainfeat_dino.pth'  --features_deep_path_test 'testfeat_dino.pth' --features_path 'binned_hcf.csv' --save_path '/MIL_experiment/ABMIL' --dropout_patch $1 --num_epochs $2 --weight_decay $3 --lr $4 --top_k $6  --organ '' --gpu_index 1    --use_additive 'yes'  --dropout_node $5  --no_projection 'yes'   --feat_type 'patchattn_with_featattn_mlpmixer'  --stop_gradient 'yes'  --cross_val_fold $7 --temperature 3.0	 &
+
+
+
 }
 
 
