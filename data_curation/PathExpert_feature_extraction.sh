57bf0a7e3bba62827c517e42e8a3555505a1bf8f
Saarthak Kapse <44542181+saarthak-kapse@users.noreply.github.com>
2024-05-13 12:39:26 -0400

    Update PathExpert_feature_extraction.sh

--- data_curation/PathExpert_feature_extraction.sh
+++ data_curation/PathExpert_feature_extraction.sh
@@ -6,15 +6,14 @@ python deepzoom_tiler_organ.py --dataset "$parent_dir/slides" --save_path "$pare
 
 python patch_dict_list.py --patch_path "$parent_dir/patches"
 
-python extract_cell_statistics_features.py --data_path "$parent_dir/slides" --cell_properties_path "$parent_dir/cell_property" --list_dict_path "$parent_dir/patches" --save_path "$parent_dir/Handcrafted_features/cell_statistics"  --workers 10
+python extract_cell_statistics_features.py --data_path "$parent_dir/slides" --cell_properties_path "$parent_dir/cell_property" --list_dict_path "$parent_dir/patches" --save_path "$parent_dir/features/cell_statistics"  --workers 10
 
-python extract_sna_features.py --data_path "$parent_dir/slides" --cell_properties_path "$parent_dir/cell_property" --list_dict_path "$parent_dir/patches" --save_path "$parent_dir/Handcrafted_features/sna_statistics"  --workers 10
+python extract_sna_features.py --data_path "$parent_dir/slides" --cell_properties_path "$parent_dir/cell_property" --list_dict_path "$parent_dir/patches" --save_path "$parent_dir/features/sna_statistics"  --workers 10
 
-python extract_athena_spatial_features.py --data_path "$parent_dir/slides" --cell_properties_path "$parent_dir/cell_property" --list_dict_path "$parent_dir/patches" --save_path "$parent_dir/Handcrafted_features/athena_statistics"  --workers 10
+python extract_athena_spatial_features.py --data_path "$parent_dir/slides" --cell_properties_path "$parent_dir/cell_property" --list_dict_path "$parent_dir/patches" --save_path "$parent_dir/features/athena_statistics"  --workers 10
 
-python extract_tissue_features.py --data_path "$parent_dir/slides" --hovernet_json_path "$parent_dir/Hovernet_output/json" --list_dict_path "$parent_dir/patches" --save_path "$parent_dir/Handcrafted_features/tissue_statistics"  --workers 10 --background_threshold 220
+python extract_tissue_features.py --data_path "$parent_dir/slides" --hovernet_json_path "$parent_dir/Hovernet_output/json" --list_dict_path "$parent_dir/patches" --save_path "$parent_dir/features/tissue_statistics"  --workers 10 --background_threshold 220
 
-python club_features.py --feat_path "$parent_dir/Handcrafted_features" --column_name_path "$parent_dir" --list_dict_path "$parent_dir/patches" --remove_cell_type "none"
+python club_features.py --feat_path "$parent_dir/features" --column_name_path "$parent_dir" --list_dict_path "$parent_dir/patches" --remove_cell_type "none"
 
-python data_filtering.py --feat_path "$parent_dir/Handcrafted_features" --save_path "$parent_dir/Handcrafted_features" --train_test_dict_path "$parent_dir/train_test_dict.json" --list_dict_path "$parent_dir/patches" --bins 10 --norm_feat "bin" --remove_noneoplastic "False"
-
+python data_filtering.py --feat_path "$parent_dir/features" --save_path "$parent_dir/features" --train_test_dict_path "$parent_dir/train_test_dict.json" --list_dict_path "$parent_dir/patches" --bins 10 --norm_feat "bin" --remove_noneoplastic "False"
